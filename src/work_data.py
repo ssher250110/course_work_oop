12786c434af677923e951c86bbbac791b43d7e63
Sergey Shershikov <ssher250110@yandex.ru>
2024-02-13 01:34:22 +0300

    editing code

--- src/work_data.py
+++ src/work_data.py
@@ -1,4 +1,5 @@
 import json
+import os
 from abc import ABC, abstractmethod
 
 from src.work_vacancy import Vacancy
@@ -7,53 +8,50 @@ from src.work_vacancy import Vacancy
 class Saver(ABC):
     def __init__(self, path):
         self.path = path
-        with open(self.path, "w") as file:
-            json.dump([], file)
 
     @abstractmethod
-    def add_vacancies(self, vacancies: list[Vacancy]) -> None:
+    def add_vacancies_file(self, vacancies: list[Vacancy]) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def get_vacancies(self, queries=None) -> list[dict]:
+    def get_vacancies_file(self) -> list[dict]:
         raise NotImplementedError
 
     @abstractmethod
-    def delete_vacancies(self, query=None) -> None:
+    def delete_vacancies_file(self) -> None:
         raise NotImplementedError
 
 
 class JsonSaver(Saver):
 
-    def add_vacancies(self, vacancies: list[Vacancy]) -> None:
+    def add_vacancies_file(self, vacancies: list[Vacancy]) -> None:
         """
         Добавление вакансий в файл json
         :param vacancies: список экземпляров класса вакансий
         """
         vacancies_json = [vacancy.to_dict() for vacancy in vacancies]
-        with open(self.path, encoding="utf-8") as file:
-            old_vacancies: list[dict] = json.load(file)
-        old_vacancies.extend(vacancies_json)
-        with open(self.path, "w", encoding="utf-8") as file:
-            json.dump(old_vacancies, file, ensure_ascii=False, indent=4)
+        with open(self.path, 'a', encoding="utf-8") as file:
+            if os.stat(self.path).st_size == 0:
+                json.dump(vacancies_json, file, ensure_ascii=False, indent=4)
+            else:
+                with open(self.path, "r", encoding="utf-8") as file:
+                    old_vacancies: list[dict] = json.load(file)
+                    old_vacancies.extend(vacancies_json)
+                with open(self.path, "w", encoding="utf-8") as file:
+                    json.dump(old_vacancies, file, ensure_ascii=False, indent=4)
 
-    def get_vacancies(self, queries=None) -> list[dict]:
+    def get_vacancies_file(self) -> list[dict]:
         """
         Получение вакансий из файла json
-        :param queries: необходимый запрос данных
+        :return: список словарей вакансий
         """
-        with open(self.path, encoding="utf-8") as file:
+        with open(self.path, "r", encoding="utf-8") as file:
             all_vacancies: list[dict] = json.load(file)
-        vacancies = []
-        # for vacancy in all_vacancies:
-        #     if all(vacancy.get(field) == query for field, query in queries.items()):
-        #         vacancies.append(vacancy)
+        return all_vacancies
 
-        for field, query in queries.items():
-            for vacancy in all_vacancies:
-                if vacancy[field] == query:
-                    vacancies.append(vacancy)
-        return vacancies
-
-    def delete_vacancies(self, query=None) -> None:
-        pass
+    def delete_vacancies_file(self) -> None:
+        """
+        Удаление вакансий из файла json
+        """
+        with open(self.path, "w", encoding="utf-8") as file:
+            file.write("")
